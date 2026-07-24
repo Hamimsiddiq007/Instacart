c5414816b860d168781213bab59eb88a2c9bf8de
Hamim <hamimsiddiq915@gmail.com>
2026-07-24 21:10:50 +0600

    toaster setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-
+import { Toaster } from "react-hot-toast"
 
 const App = () => {
   return (
-    <div>
-     App
-    </div>
+    <>
+    <Toaster position="top-right" toastOptions={{duration: 3000, style: {background: '#1B3022', color: '#fff', borderRadius: '12px', fontSize: '14px'}}} />
+     
+    </>
   )
 }
 
