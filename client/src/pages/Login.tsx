d4a92e7372423c36ec6b5b6c89f66ad71a1d8e1d
Hamim <hamimsiddiq915@gmail.com>
2026-07-25 20:06:56 +0600

    submit button

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { heroSectionData } from "../assets/assets";
 import { Link } from "react-router-dom";
-import { BikeIcon, LockIcon, MailIcon, UserIcon } from "lucide-react";
+import { BikeIcon, Loader2Icon, LockIcon, MailIcon, UserIcon } from "lucide-react";
 
 const Login = () => {
   const [loginState, setLoginState] = useState(true);
@@ -108,6 +108,9 @@ const Login = () => {
                   />
                 </div>
               </label>
+              <button type="submit" disabled={loading} className="flex-center w-full py-3 bg-green-950 text-white font-semibold rounded-xl hover:bg-green-900 transition-colors disabled:opacity-50">
+                {loading ? <Loader2Icon className="animate-spin"/> : loginState ? "Sign in" : "Sign up"}
+              </button>
           </form>
         </div>
       </div>
