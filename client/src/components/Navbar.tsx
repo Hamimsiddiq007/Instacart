ccdf4ae542d20839342877b55822b1484e8604b1
Hamim <hamimsiddiq915@gmail.com>
2026-07-26 20:08:55 +0600

    nav logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,23 +1,36 @@
+import { BikeIcon } from "lucide-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const user: any = {
+    name: "Avinash",
+    email: "admin@example.com",
+    isAdmin: true,
+  };
+  const { cartCount, setIsCartOpen } = {
+    cartCount: 5,
+    setIsCartOpen: (_data: any) => {},
+  };
+  const [searchQuery, setSearchQuery] = useState("");
+  const [userManuOpen, setUserManuOpen] = useState(false);
 
-    const user: any = {name: "Avinash", email: "admin@example.com", isAdmin: true};
-    const {cartCount, setIsCartOpen} = {
-        cartCount: 5,
-        setIsCartOpen: (_data: any) => {}
-    }
-    const [searchQuery, setSearchQuery] = useState("");
-    const [userManuOpen, setUserManuOpen] = useState(false);
-
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <div>
-      Navbar component
-    </div>
-  )
-}
+    <nav className="bg-white sticky top-0 z-50 border-b border-app-border">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 gap-4">
+        <Link
+          to="/"
+          className="flex items-center gap-2 text-[22px] font-medium shrink-0"
+        >
+          <BikeIcon size={24} className="" />
+          <span>Instacart</span>
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
