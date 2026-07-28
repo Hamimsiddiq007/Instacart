254d84fdf7ef74873a76b91a2152115dda422865
Hamim <hamimsiddiq915@gmail.com>
2026-07-28 21:39:16 +0600

    feat: browse category

--- client/src/components/HomeCategories.tsx
+++ client/src/components/HomeCategories.tsx
@@ -16,6 +16,7 @@ const HomeCategories = () => {
                     <div className="size-18 sm:size-26 sm:p-2 rounded-2xl overflow-hidden bg-orange-100 group-hover:ring-2 ring-orange-300/75 transition-all">
                         <img src={category.image} alt={category.name} className="w-full h-full object-contain rounded-full transition-all" />
                     </div>
+                    <span className="text-xs font-medium text-zinc-600 text-center leading-tight">{category.name}</span>
                 </Link>
             ))}
         </div>
