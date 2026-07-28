a096e8f39b938ef95190c99d1087e44e8474fa65
Hamim <hamimsiddiq915@gmail.com>
2026-07-28 21:35:32 +0600

    homeCategory image

--- client/src/components/HomeCategories.tsx
+++ client/src/components/HomeCategories.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+import { categoriesData } from "../assets/assets"
 
 
 const HomeCategories = () => {
@@ -7,6 +9,15 @@ const HomeCategories = () => {
         <div className="">
             <h2 className="text-2xl font-semibold">Browse Categories</h2>
             <p className="text-sm text-app-text-light mt-1">Find exactly what you need using</p>
+        </div>
+        <div className="flex items-center mt-8 overflow-x-scroll no-scrollbar">
+            {categoriesData.map((category) => (
+                <Link key={category.slug} to={`/products?category=${category.slug}`} onClick={() => window.scrollTo(0, 0)} className="group flex flex-col items-center gap-3 p-4">
+                    <div className="size-18 sm:size-26 sm:p-2 rounded-2xl overflow-hidden bg-orange-100 group-hover:ring-2 ring-orange-300/75 transition-all">
+                        <img src={category.image} alt={category.name} className="w-full h-full object-contain rounded-full transition-all" />
+                    </div>
+                </Link>
+            ))}
         </div>
       </div>
     </section>
