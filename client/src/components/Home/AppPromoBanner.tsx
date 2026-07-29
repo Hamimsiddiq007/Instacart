95b0a4228d86efe8efeefb528f8a6d638a445ad0
Hamim <hamimsiddiq915@gmail.com>
2026-07-29 20:47:19 +0600

    promrBanner right

--- client/src/components/Home/AppPromoBanner.tsx
+++ client/src/components/Home/AppPromoBanner.tsx
@@ -1,11 +1,11 @@
-import { appPromoBannerData } from "../../assets/assets"
+import { appPromoBannerData, assets } from "../../assets/assets"
 
 
 const AppPromoBanner = () => {
   return (
     <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20 my-14 bg-green-950 rounded-2xl">
-      {/* Leftside Content */}
       <div className="flex flex-col md:flex-row items-center justify-between gap-8 xl:px-10">
+        {/* Leftside Content */}
         <div className="text-center md:text-left">
             <h2 className="font-serif text-3xl sm:text-4xl text-white mb-3">{appPromoBannerData.title}</h2>
             <p className="text-white/70 mb-6 max-w-md">{appPromoBannerData.description}</p>
@@ -14,6 +14,8 @@ const AppPromoBanner = () => {
                 <button className="px-6 py-3 bg-white/10 text-white font-semibold rounded-xl hover:bg-white/20 transition-colors border border-white/20">Google Play</button>
             </div>
         </div>
+        {/* Rightside Image */}
+      <img src={assets.delivery_truck} alt="Delivery Truck" className="max-w-60 sm:max-w-120 xl:pr-10" />
       </div>
     </section>
   )
