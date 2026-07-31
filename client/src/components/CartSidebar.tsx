18850f342c74918db8dd9087284d215c0620792c
Hamim <hamimsiddiq915@gmail.com>
2026-07-31 21:33:39 +0600

    cartSidebar variables

--- client/src/components/CartSidebar.tsx
+++ client/src/components/CartSidebar.tsx
@@ -1,6 +1,19 @@
-
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../context/CartContext";
 
 const CartSidebar = () => {
+
+    const currency = import.meta.env.VITE_CURRENCY_SYMBOL || '$';
+
+    const {items, updateQuantity, removeFromCart, cartTotal, isCartOpen, setIsCartOpen} = useCart();
+
+    const navigate = useNavigate();
+
+    if(!isCartOpen) return null;
+
+    const delivaryFee = cartTotal > 20 ? 0 : 1.99;
+    const  grandTotal = cartTotal + delivaryFee;
+
   return (
     <>
       {/* Overlay */}
