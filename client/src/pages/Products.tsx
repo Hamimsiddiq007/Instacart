2a0b2c4b9f12dec9a3cb429f598857a180c2e437
Hamim <hamimsiddiq915@gmail.com>
2026-08-02 19:41:50 +0600

    productPage variables

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import type { Product } from "../types";
+
 const Products = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [products, setProducts] = useState<Product[]>([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
+
+  const category = searchParams.get("category") || "";
+  const organic = searchParams.get("organic") || "";
+  const sort = searchParams.get("sort") || "";
+  const page = Number(searchParams.get("page")) || 1;
+  const minPrice = searchParams.get("minPrice") || "";
+  const maxPrice = searchParams.get("maxPrice") || "";
+
   return (
     <div>
       Products
