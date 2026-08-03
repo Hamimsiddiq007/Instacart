6d0894092e61d486f00dc3aa6d437425cae7c885
Hamim <hamimsiddiq915@gmail.com>
2026-08-03 18:53:24 +0600

    mobile filter button

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -3,7 +3,7 @@ import { useSearchParams } from "react-router-dom";
 import type { Product } from "../types";
 import { categoriesData, dummyProducts } from "../assets/assets";
 import {Link} from "react-router-dom";
-import { Home } from "lucide-react";
+import { Home, SlidersHorizontal } from "lucide-react";
 
 const Products = () => {
 
@@ -75,6 +75,13 @@ const Products = () => {
               <div className="">
                 <h1 className="text-2xl font-semibold text-app-green">{activeCategory ? activeCategory.name : "All Products"}</h1>
                 <p className="text-sm text-app-text-light mt-0.5">{products.length} products found</p>
+              </div>
+
+              <div className="flex flex-col lg:items-center gap-3">
+                {/* Mobile Filters */}
+                <button onClick={() => setMobileFiltersOpen(true)} className="lg:hidden flex items-center gap-2 px-3 py-2 text-sm bg-white rounded-xl border border-app-border hover:bg-app-cream transition-colors">
+                  <SlidersHorizontal className="size-4" /> Filters
+                </button>
               </div>
             </div>
           </main>
