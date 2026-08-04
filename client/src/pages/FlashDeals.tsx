0ea72c9d6d4f981099886c5d387ea79c3e56c5f4
Hamim <hamimsiddiq915@gmail.com>
2026-08-04 19:44:39 +0600

    flashDeals variables

--- client/src/pages/FlashDeals.tsx
+++ client/src/pages/FlashDeals.tsx
@@ -1,6 +1,17 @@
-
+import { useEffect, useState } from "react"
+import type { Product } from "../types"
+import { dummyProducts } from "../assets/assets"
 
 const FlashDeals = () => {
+
+  const [products, setProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setProducts(dummyProducts.filter((p: any) => p.stock > 0))
+    setLoading(false)
+  }, [])
+
   return (
     <div>
       Flash Deals
