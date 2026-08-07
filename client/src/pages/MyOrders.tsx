300f6bf9760ab80224e39a90f40649b7c3dd3800
Hamim <hamimsiddiq915@gmail.com>
2026-08-07 23:15:34 +0600

    myOrders variables

--- client/src/pages/MyOrders.tsx
+++ client/src/pages/MyOrders.tsx
@@ -1,6 +1,34 @@
-
+import { useEffect, useState } from "react";
+import type { Order } from "../types";
+import { useSearchParams } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import { dummyDashboardOrdersData } from "../assets/assets";
 
 const MyOrders = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY_SYMBOL || "$";
+
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [activeTab, setActiveTab] = useState("all");
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const tab = ["all", "Placed", "Out for Delivery", "Delivered"];
+  const {clearCart} = useCart();
+
+  const fetchOrders = async () => {
+    setOrders(dummyDashboardOrdersData as any)
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    if(searchParams.get("clearCart") === "true") {
+      clearCart();
+      setSearchParams({})
+      fetchOrders();
+    }
+  }, [activeTab])
+
   return (
     <div>
       My Orders
