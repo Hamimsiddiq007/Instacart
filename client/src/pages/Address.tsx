7a0d517236269275d2157bb569208f0b46efb2f5
Hamim <hamimsiddiq915@gmail.com>
2026-08-14 21:08:58 +0600

    address variables

--- client/src/pages/Address.tsx
+++ client/src/pages/Address.tsx
@@ -1,6 +1,57 @@
-
+import { useEffect, useState } from "react";
+import type { Address } from "../types";
+import { dummyAddressData } from "../assets/assets";
 
 const Address = () => {
+
+  const [addresses, setAddresses] = useState<Address[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [showForm, setShowForm] = useState(false);
+  const [editigId, setEditingId] = useState<string | null>(null);
+  const [form, setform] = useState({
+    label: "",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+    isDefault: false
+  });
+
+  const resetForm = () => {
+    setform({
+      label: "",
+      address: "",
+      city: "",
+      state: "",
+      zip: "",
+      isDefault: false
+    });
+    setEditingId(null);
+    setShowForm(false);
+  }
+
+  const handleSubmit = async (e : React.SubmitEvent) => {
+    e.preventDefault();
+  }
+
+  const onEditHandler = (add: Address) => {
+    setform({
+      label: add.label,
+      address: add.address,
+      city: add.city,
+      state: add.state,
+      zip: add.zip,
+      isDefault: add.isDefault
+    });
+    setEditingId(add._id);
+    setShowForm(true);
+  }
+
+  useEffect(() => {
+    setAddresses(dummyAddressData)
+    setLoading(false);
+  }, []);
+
   return (
     <div>
       Address
