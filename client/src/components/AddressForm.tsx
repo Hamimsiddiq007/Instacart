30fd995a940440b885efd357ccf4d1941aeb85d0
Hamim <hamimsiddiq915@gmail.com>
2026-08-26 21:27:26 +0600

    addressForm

---
+++ client/src/components/AddressForm.tsx
@@ -0,0 +1,12 @@
+
+const AddressForm = ({resetForm, handleSubmit, form, setForm, editingId} : any) => {
+  return (
+    <>
+    {/* Overley */}
+    <div onClick={resetForm} className="fixed inset-0 bg-black/40 z-50" />
+      
+    </>
+  )
+}
+
+export default AddressForm
