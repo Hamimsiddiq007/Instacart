61a7078954c895122d266771bc1053965387767b
Hamim <hamimsiddiq915@gmail.com>
2026-08-26 21:08:17 +0600

    addressCard left

--- client/src/components/AddressCard.tsx
+++ client/src/components/AddressCard.tsx
@@ -1,3 +1,4 @@
+import { CheckIcon, MapPinIcon } from "lucide-react";
 import type { Address } from "../types"
 
 interface AddressCardProps {
@@ -11,8 +12,24 @@ const AddressCard = ({addr, onEditHandler, setAddress } : AddressCardProps) => {
         console.log(id)
     }
   return (
-    <div>
-      
+    <div key={addr._id} className="max-w-3xl bg-white rounded-2xl p-6 flex items-start justify-between">
+      {/* Left */}
+      <div className="flex gap-4">
+        <div className="size-10 rounded-xl bg-app-cream flex-center shrink-0">
+          <MapPinIcon className="size-5 text-app-green" />
+        </div>
+        <div className="">
+          <div className="flex items-center gap-2 mb-1">
+            <p className="text-sm font-semibold text-app-green">{addr.label}</p>
+            {addr.isDefault && (
+              <span className="flex-center gap-1 px-2.5 py-0.5 text-[10px] font-medium bg-app-green text-white rounded-full">
+                <CheckIcon className="size-2.5" /> Default
+              </span>
+            )}
+          </div>
+          <p className="text-sm text-app-text-light">{addr.address}, {addr.city}, <br /> {addr.state}, {addr.zip}</p>
+        </div>
+      </div>
     </div>
   )
 }
