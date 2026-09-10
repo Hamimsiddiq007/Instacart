4e38b66b8735e3e8c536007869f0a5682b456a7e
Hamim <hamimsiddiq915@gmail.com>
2026-09-10 23:23:39 +0600

    searchResult variables

--- client/src/pages/SearchResult.tsx
+++ client/src/pages/SearchResult.tsx
@@ -1,6 +1,22 @@
-
+import { useEffect, useState } from "react";
+import type { Product } from "../types";
+import { useSearchParams } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
 
 const SearchResult = () => {
+
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("q") || "";
+
+  useEffect(()=> {
+    if(!query) return;
+    setLoading(true);
+    setProducts(dummyProducts.filter((p: any) => p.name.toLowerCase().includes(query.toLowerCase())));
+    setLoading(false);
+  },[query])
+
   return (
     <div>
       Search Result
