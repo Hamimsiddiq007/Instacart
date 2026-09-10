51c3f15d05c7760780cf0dee6c5086984b412d82
Hamim <hamimsiddiq915@gmail.com>
2026-09-11 00:14:25 +0600

    feat: searchResult

--- client/src/pages/SearchResult.tsx
+++ client/src/pages/SearchResult.tsx
@@ -18,8 +18,8 @@ const SearchResult = () => {
   },[query])
 
   return (
-    <div>
-      Search Result
+    <div className="min-h-screen bg-app-cream">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6"></div>
     </div>
   )
 }
