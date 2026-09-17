7e0c6b3b8a40fb4adfad07d59d55528902999d87
Hamim <hamimsiddiq915@gmail.com>
2026-09-17 19:47:22 +0600

    controllers

---
+++ server/controllers/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { prisma } from "../config/prisma.js";
+import bcrypt from "bcrypt";
+
+// Register
+export const register = async (req: Request, res: Response) => {
+    const {name, email, password} = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: "Please provide all fields" });
+    }
+
+    const existingUser = await prisma.user.findUnique({where: {email: email.toLowerCase()}});
+
+    if (existingUser) {
+        return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email: email.toLowerCase(),
+            password: hashedPassword,
+        }
+    })
+}
