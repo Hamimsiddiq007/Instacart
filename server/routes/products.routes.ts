0c43d75a2144628b2f88f0391ad0edd1db4e6dd7
Hamim <hamimsiddiq915@gmail.com>
2026-09-18 22:11:35 +0600

    product router

---
+++ server/routes/products.routes.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getFlashDeals,
+  getProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/products.controller.js";
+import auth from "../middlewares/auth.middleware.js";
+import admin from "../middlewares/admin.middleware.js";
+
+const productRouter = express.Router();
+
+productRouter.get("/flash-deals", getFlashDeals);
+productRouter.get("/", getProducts);
+productRouter.get("/:id", getProduct);
+productRouter.post("/", auth, admin, createProduct);
+productRouter.put("/:id", auth, admin, updateProduct);
+productRouter.delete("/:id", auth, admin, deleteProduct);
+
+export default productRouter;
