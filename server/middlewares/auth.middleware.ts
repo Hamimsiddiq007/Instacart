9bf64da335a2f59d20978a70187a39508ae0efb2
Hamim <hamimsiddiq915@gmail.com>
2026-09-18 21:55:48 +0600

    bug fixed

--- server/middlewares/auth.middleware.ts
+++ server/middlewares/auth.middleware.ts
@@ -1,20 +1,24 @@
 import { NextFunction, Request, Response } from "express";
-import  Jwt  from "jsonwebtoken";
+import Jwt from "jsonwebtoken";
 
 const auth = (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const authHeader = req.headers.authorization;
+  try {
+    const authHeader = req.headers.authorization;
 
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-            throw new Error("Authorization header is missing or invalid");
-        }
-
-        const token = authHeader.split(" ")[1];
-        const decoded = Jwt.verify(token, process.env.JWT_SECRET as string) as { id: string };
-        
-        req.user = { id: decoded.id };
-        next();
-    } catch (error) {
-        return res.status(401).json({message: "Invalid or expired token"});
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      throw new Error("Authorization header is missing or invalid");
     }
-}
+
+    const token = authHeader.split(" ")[1];
+    const decoded = Jwt.verify(token, process.env.JWT_SECRET as string) as {
+      id: string;
+    };
+
+    req.user = { id: decoded.id };
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
+
+export default auth;
