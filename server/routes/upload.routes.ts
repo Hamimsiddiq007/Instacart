eba1bdeccd37bc0daecdce8fd18d334ebfb28e7f
Hamim <hamimsiddiq915@gmail.com>
2026-09-19 21:16:34 +0600

    image upload

--- server/routes/upload.routes.ts
+++ server/routes/upload.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import auth from "../middlewares/auth.middleware.js";
 import multer from "multer";
+import cloudinary from "../config/cloudinary.js";
 
 const router = express.Router();
 
@@ -12,6 +13,15 @@ router.post("/", auth, upload.single("image"), async (req, res) => {
         if(!req.file){
             return res.status(400).json({ message: "No image uploaded" });
         }
+
+        const b64 = Buffer.from(req.file.buffer).toString("base64");
+        const dataURI = `data:${req.file.mimetype};base64,${b64}`;
+
+        const result = await cloudinary.uploader.upload(dataURI, {
+            folder: "instacart",
+            resource_type: "auto",
+        });
+
     } catch (error) {
         res.status(500).json({ message: "Error uploading image" });
     }
