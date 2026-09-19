bb7783cca2c03d8057d335bb4227aa5eb626dc63
Hamim <hamimsiddiq915@gmail.com>
2026-09-19 21:08:51 +0600

    cloudinary configaration

--- server/routes/upload.routes.ts
+++ server/routes/upload.routes.ts
@@ -9,8 +9,10 @@ const upload = multer({ storage });
 
 router.post("/", auth, upload.single("image"), async (req, res) => {
     try {
-        
+        if(!req.file){
+            return res.status(400).json({ message: "No image uploaded" });
+        }
     } catch (error) {
-        
+        res.status(500).json({ message: "Error uploading image" });
     }
 })
