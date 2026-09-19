493270c3cc310e02681f5813e06808e6467b9077
Hamim <hamimsiddiq915@gmail.com>
2026-09-19 20:47:50 +0600

    multer setup

--- server/routes/upload.routes.ts
+++ server/routes/upload.routes.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import auth from "../middlewares/auth.middleware.js";
+import multer from "multer";
+
 const router = express.Router();
 
-router.post("/", auth, async (req, res) => {
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.post("/", auth, upload.single("image"), async (req, res) => {
     try {
         
     } catch (error) {
