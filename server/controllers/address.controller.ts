271036b5c0fe2f061ee412ac8abe54cbed3e87b4
Hamim <hamimsiddiq915@gmail.com>
2026-09-25 19:10:43 +0600

    getAddress controller

--- server/controllers/address.controller.ts
+++ server/controllers/address.controller.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
+import { prisma } from "../config/prisma.js";
 
 
 // Get user address
 export const getUserAddress = async (req: Request, res: Response) => {
-    
+    const addresses = await prisma.address.findMany({
+        where: {userId: req.user!.id},
+        orderBy: {createdAt: "asc"}
+    })
+    res.json({addresses})
 }
