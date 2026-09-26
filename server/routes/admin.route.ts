f1dbf3408ec7c0fa1025502879f56a31851fe4ac
Hamim <hamimsiddiq915@gmail.com>
2026-09-26 21:32:22 +0600

    admin routes

---
+++ server/routes/admin.route.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import auth from "../middlewares/auth.middleware.js";
+import admin from "../middlewares/admin.middleware.js";
+import {
+  assignDeliveryPartner,
+  createDeliveryPartner,
+  getAdminStats,
+  getDeliveryPartners,
+  updateDeliveryPartner,
+} from "../controllers/admin.controller.js";
+
+const adminRouter = express.Router();
+
+adminRouter.get("/stats", auth, admin, getAdminStats);
+adminRouter.get("/delivery-partners", auth, admin, getDeliveryPartners);
+adminRouter.post("/delivery-partners", auth, admin, createDeliveryPartner);
+adminRouter.put("/delivery-partners/:id", auth, admin, updateDeliveryPartner);
+adminRouter.put("/orders/:id/assign", auth, admin, assignDeliveryPartner);
+
+export default adminRouter;
